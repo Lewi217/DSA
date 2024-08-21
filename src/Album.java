df3664ba67681dbd966c73bd586fc36377f82f7c
wanjohi lewis <wanjohilewi7@gmail.com>
2024-08-21 23:14:53 +0300

    Linked list challenge from udemy

---
+++ src/Album.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Album {
+    public String name;
+    private String artist;
+    private ArrayList<Song> songs;
+
+    public Album(String name, String artist, ArrayList<Song> songs) {
+        this.name = name;
+        this.artist = artist;
+        this.songs = new ArrayList<Song>();
+    }
+
+    public boolean addSong(String title, double duration){
+        if(findSong(title) ==null ) {
+            this.songs.add(new Song(title, duration));
+            return true;
+        }
+        return false;
+    }
+
+    public Song findSong(String title) {
+        for(Song checkedSong: this.songs){
+
+        }
+    }
+}
+
+
+
